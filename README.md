66be2c3b9a6d65234d77f3eefd296c855c93207e
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2023-11-27 23:56:51 -0500

    Mobile

---
+++ README.md
@@ -0,0 +1,27 @@
+# NEWS HOMEPAGE
+
+- This project is based on one of the [Frontend Mentor](https://www.frontendmentor.io/) challenges.
+
+## Screenshots
+- VIEW: Desktop
+
+![Desktop](./screenshot-desktop.png)
+
+- VIEW: Mobile
+
+![Mobile](./screenshot-mobile.png)
+
+- VIEW: Mobile con menu 
+
+![MobileMenu](./screenshot-mobile-menu.png)
+
+### Mobile
+
+### [News homepage](https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl)
+
+
+# Technologies used
+
+- Html
+- Css
+- Javascript
--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const navMobile = document.getElementById('nav-mobile');
+const contentMain = document.getElementById('main');
 
+const menuIconClosed = document.getElementById('menu-closed');
+const menuIconOpend = document.getElementById('menu-opend');
+
+const navItems = document.querySelectorAll('.item');
+
+const MENU_ACTIONS = {
+  OPEND: 'MENU-OPEND',
+  CLOSED: 'MENU-CLOSED',
+  SECTION: 'MENU-SELECT-SECTION'
+}
+
+const toggleMenu = (ACTION, ITEM) => {
+  if (MENU_ACTIONS.OPEND === ACTION) {
+    navMobile.classList.remove('nav-hidden');
+    contentMain.classList.add('main-w-menu');
+  }
+  else if (MENU_ACTIONS.CLOSED === ACTION || MENU_ACTIONS.SECTION === ACTION) {
+    navMobile.classList.add('nav-hidden');
+    contentMain.classList.remove('main-w-menu');
+  }
+
+  if (MENU_ACTIONS.SECTION === ACTION) {
+    const idContainer = ITEM.textContent.toLowerCase();
+    const container = document.getElementById(idContainer);
+    container.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
+  }
+};
+
+navItems.forEach(item => item.addEventListener('click', () => toggleMenu(MENU_ACTIONS.SECTION, item)))
+
+menuIconOpend.addEventListener('click', () => toggleMenu(MENU_ACTIONS.OPEND));
+menuIconClosed.addEventListener('click', () => toggleMenu(MENU_ACTIONS.CLOSED));
